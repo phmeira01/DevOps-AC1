9b62ca5515df88e464fa109b893cb40e3cbca32d
phmeira01 <69548791+phmeira01@users.noreply.github.com>
2020-10-09 15:55:29 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -13,24 +13,3 @@ def main( ):
 
 if _name_ == "_main_":
 	main()
-o conteudo do arquivo testes.py é 
-import jogovelha
-import sys
-
-erroInicializar = False
-jogo = jogovelha.inicializar()
-
-if len(jogo) != 3:
-	erroInicializar = True
-else:
-	for linha in jogo:
-		if len(linha) != 3:
-			erroInicializar = True
-		else:
-			for elemento in linha:
-				if elemento != '.':
-					erroInicializar = True
-if erroInicializar:
-	sys.exit(1)
-else:
-	sys.exit(0)
